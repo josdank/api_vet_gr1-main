9411d3d1074ac7dfb009db20d8706af638eea191
Luis Adrian Ramos <luchoramos042004@gmail.com>
2024-12-16 13:20:03 -0500

    pacientes

---
+++ src/models/Paciente.js
@@ -0,0 +1,11 @@
+import {Schema, model} from "mongoose";
+import bcrypt from "bcrypt.js";
+
+//SChemas
+const pacienteSchema = new Schema()
+
+
+
+
+// Creación moidelo y su exportación 
+export default model('Paciente', pacienteSchema)
